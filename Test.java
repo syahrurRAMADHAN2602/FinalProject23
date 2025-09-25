985631f5cb41293e3a927b7ade7b34b490bf8f14
Firmansyah <145847595+Fxzeee@users.noreply.github.com>
2025-09-25 10:13:39 +0700

    kedua

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
         
         System.out.println("Hai, ini baris kode pertama");
+        System.out.println("Hai, aku Firman");
 
     }
 
